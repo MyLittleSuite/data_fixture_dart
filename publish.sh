0d7bacbaafaf6dce716d39f25112db2d534d4def
Andrea Del Fante <andreadelfante94@gmail.com>
2020-11-23 23:21:11 +0100

    Fix publish.sh

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 flutter pub publish --dry-run
 
 VERSION=$(grep 'version:' pubspec.yaml | awk '{ print $2 }' | cut -d '+' -f 1 | tr -d '\n')
-git tag -a "$VERSION"
+git tag -a "$VERSION" -m "$VERSION"
 git push origin --tags
 
 flutter pub publish
